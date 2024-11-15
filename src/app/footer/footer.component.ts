966b025aa7aefa795264d89bd7867ee908d78b6d
Rade Spasojevic <rspasojevic@inwerken.de>
2024-11-15 13:35:02 +0100

    Added missing icons in the footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { AutomIconComponent } from "../shared/components/autom-icon/autom-icon.component";
+
+// Enums
+import { ColorEnum, IconsEnum } from '../shared/data-models/enums';
 
 @Component({
   selector: 'autom-footer',
   standalone: true,
-  imports: [],
+  imports: [AutomIconComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  colorEnum = ColorEnum;
+  iconEnum = IconsEnum;
 
 }
