ae57321373496996a64a0e0d043427023758eece
Rade Spasojevic <rspasojevic@inwerken.de>
2024-12-18 22:00:39 +0100

    Added right empty container

--- src/app/modules/webshop/webshop.component.ts
+++ src/app/modules/webshop/webshop.component.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
 import { InputFieldsComponent } from '../../shared/components/input-fields/input-fields.component';
-import { ButtonThemes, ButtonTypes, ColorEnum, IconsEnum, SizeEnum } from '../../shared/data-models/enums';
+import {
+  ButtonThemes,
+  ButtonTypes,
+  ColorEnum,
+  IconsEnum,
+  SizeEnum,
+} from '../../shared/data-models/enums';
 import { ButtonComponent } from '../../shared/components/button/button.component';
 import { CommonModule } from '@angular/common';
+import { AutomIconComponent } from '../../shared/components/autom-icon/autom-icon.component';
 
 @Component({
   selector: 'app-webshop',
   standalone: true,
-  imports: [InputFieldsComponent, ButtonComponent, CommonModule],
+  imports: [
+    InputFieldsComponent,
+    ButtonComponent,
+    CommonModule,
+    AutomIconComponent,
+  ],
   templateUrl: './webshop.component.html',
-  styleUrl: './webshop.component.scss'
+  styleUrl: './webshop.component.scss',
 })
 export class WebshopComponent {
-
   // Enums
   sizeEnum = SizeEnum;
   iconEnum = IconsEnum;
